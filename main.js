c254882fe0be1a9860b3bef576762766e25bc71a
lior2911 <aytegev1@gmail.com>
2022-05-23 23:19:41 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,109 @@
+// DATES
+
+// צרו פונקצייה שמדפיסה לקונסול את התאריך והשעה בעת הפעלתה
+
+// function someDate(){
+//   var myDate = new Date()
+//   console.log(myDate)
+// }
+// someDate()
+
+// function someDate(myDate){
+//   var myDate = new Date("12/10/2009")
+
+//   console.log(myDate)
+// }
+// someDate("1/2/1992")
+
+// function getDateFromUser(realDate){
+//   var userString = prompt("enter a date")
+//   var realDate = new Date()
+//   console.log(realDate)
+// }
+// getDateFromUser()
+
+// function someDate(newUserDate){
+//   var newUserDate =new Date(prompt("enter a date")) ;
+//   return newUserDate.getMonth();
+// }
+// console.log(someDate())
+
+// function someHour (userHour){
+//   var userHour = new Date (prompt());
+//   return userHour.getHours();
+// }
+// console.log(someHour())
+
+// function someYear(){
+//   var userYear = new Date(prompt());
+//   return userYear.getFullYear();
+// }
+// console.log(someYear())
+
+// function getMinuteFromUser(){
+//   var userMinute = new Date(prompt());
+//   return userMinute.getMinutes();
+// }
+// console.log(getMinuteFromUser())
+
+// function someDate(){
+//   var date1 = new Date(prompt());
+//   var date2 = new Date(prompt());
+//   if (date1<date2){
+//     return date1;
+//   }
+//   return date2;
+  
+// }
+// console.log(someDate())
+
+// function getDateFromUser(){
+//   
+//   var userDate = new Date(prompt("enter birth date"));
+//   var userAge = new Date().getFullYear() - userDate.getFullYear();
+// return userAge;
+// }
+// console.log(getDateFromUser())
+
+// function getInOrGetOut(){
+//   var userAge = new Date(prompt());
+//   var localDate = new Date().getFullYear() - userAge.getFullYear();
+//   if(localDate>=18){
+//     alert("hello mother fucker come in")
+//   }
+//   else{
+//     console.log("get out of here ya bish")
+//   }
+
+// }
+// console.log(getInOrGetOut())
+
+function getLoc(){
+  var myDate = new Date()
+  
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function getRandom(){
+//   var myDate = new Date();
+//   var rndDay = Math.floor(Math.random()*(31 - 1)+1)
+//   var rndMonth = Math.floor(Math.random()*(2022-1990)+1990)
+
+
+
+
+  // return myDate;
+
+
+ 
